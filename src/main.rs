e7315af458ec5ccc1e03e51f68f2e5f6c067288c
Maximilian Dollinger <maximilian.dollinger@haufe-lexware.net>
2026-08-11 17:46:18 +0200

    add: write test file

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,151 @@
-use std::{error::Error, sync::atomic::AtomicU64};
+mod record;
 
-use crc32c::crc32c;
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, BufReader, Read, Write},
+    sync::atomic::{AtomicU16, AtomicU64},
+    time::Instant,
+};
 
-#[derive(Debug)]
-struct Record<'a> {
-    magic: u8,
-    seq: u64,
-    txn: u64,
-    txn_cnt: u16,
-    schema_seq: u64,
-    data: &'a [u8],
+use crate::record::Record;
+
+static REC_SEQ: AtomicU64 = AtomicU64::new(1);
+static FILE_SEQ: AtomicU16 = AtomicU16::new(1);
+
+struct DbFile {
+    seq: u16,
+    file: File,
+    written: usize,
+    min_seq: u64,
+    max_seq: u64,
     hash: u32,
 }
 
-impl<'a> Record<'a> {
-    fn new(txn: u64, txn_cnt: u16, schema_seq: u64, data: &'a [u8]) -> Self {
-        let seq = SEQ.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+impl DbFile {
+    pub const FILE_SIZE: usize = 1000 * 1024 * 1024;
 
-        Record {
-            magic: b'R',
+    pub fn new(rec_seq: u64) -> io::Result<Self> {
+        let seq = FILE_SEQ.fetch_and(1, std::sync::atomic::Ordering::SeqCst);
+        let name = format!("{seq}.breezy");
+        let mut file = OpenOptions::new()
+            .create(true)
+            .truncate(false)
+            .write(true)
+            .read(true)
+            .open(&name)?;
+
+        file.set_len(DbFile::FILE_SIZE as u64)?;
+
+        file.write_all(&seq.to_le_bytes())?;
+
+        Ok(DbFile {
             seq,
-            txn,
-            txn_cnt,
-            schema_seq,
-            data,
+            file,
+            written: 8,
+            min_seq: rec_seq,
+            max_seq: u64::MIN,
             hash: 0,
-        }
-    }
-
-    pub fn from_slice(src: &'a [u8]) -> Result<Self, Box<dyn Error>> {
-        let mut cursor = 0;
-        let magic: u8 = u8::from_le_bytes(src[cursor..cursor + 1].try_into()?);
-        cursor += 1;
-
-        if magic != b'R' {
-            return Err("Not valid record magic number".into());
-        }
-
-        let len = u32::from_le_bytes(src[cursor..cursor + 4].try_into()?);
-        cursor += 4;
-        let seq = u64::from_le_bytes(src[cursor..cursor + 8].try_into()?);
-        cursor += 8;
-        let txn = u64::from_le_bytes(src[cursor..cursor + 8].try_into()?);
-        cursor += 8;
-        let txn_cnt = u16::from_le_bytes(src[cursor..cursor + 2].try_into()?);
-        cursor += 2;
-        let schema_seq = u64::from_le_bytes(src[cursor..cursor + 8].try_into()?);
-        cursor += 8;
-        let data = &src[cursor..(len as usize - 4)];
-        cursor = len as usize - 4;
-        let hash = u32::from_le_bytes(src[cursor..cursor + 4].try_into()?);
-
-        Ok(Record {
-            magic,
-            seq,
-            txn,
-            txn_cnt,
-            schema_seq,
-            data,
-            hash,
         })
     }
 
-    fn len(&self) -> usize {
-        1 + 4 + 8 + 8 + 2 + 8 + self.data.len() + 4
+    pub fn write(&mut self, data: &[u8], seq: u64) -> io::Result<()> {
+        self.file.write_all(data)?;
+        self.file.sync_all()?;
+
+        self.written += data.len();
+        self.max_seq = seq;
+
+        Ok(())
     }
 
-    fn write(&self, target: &mut [u8]) -> Result<usize, Box<dyn Error>> {
-        let len: usize = self.len();
+    pub fn has_space(&self, len: usize) -> bool {
+        self.written + len < DbFile::FILE_SIZE - 20
+    }
 
-        if target.len() < len {
-            return Err(format!(
-                "write targte has not enough space! wanted: {}bytes; actual: {}bytes",
-                len,
-                target.len()
-            )
-            .into());
+    pub fn seal(&mut self) -> io::Result<()> {
+        let mut footer_buf = [0_u8; 20];
+        footer_buf[0..8].copy_from_slice(&self.min_seq.to_le_bytes());
+        footer_buf[8..16].copy_from_slice(&self.max_seq.to_le_bytes());
+
+        let mut reader = BufReader::with_capacity(2 * 1024 * 1024, &self.file);
+        let mut crc = 0u32;
+        let mut buf = [0u8; 1024 * 1024];
+
+        loop {
+            let n = reader.read(&mut buf)?;
+            if n == 0 {
+                break;
+            }
+            crc = crc32c::crc32c_append(crc, &buf[..n]);
         }
 
-        target[0..1].copy_from_slice(&self.magic.to_le_bytes());
-        target[1..5].copy_from_slice(&(len as u32).to_le_bytes());
-        target[5..13].copy_from_slice(&self.seq.to_le_bytes());
-        target[13..21].copy_from_slice(&self.txn.to_le_bytes());
-        target[21..23].copy_from_slice(&self.txn_cnt.to_le_bytes());
-        target[23..31].copy_from_slice(&self.schema_seq.to_le_bytes());
-        target[31..(31 + self.data.len())].copy_from_slice(self.data);
+        footer_buf[16..20].copy_from_slice(&crc.to_le_bytes());
 
-        let hash = crc32c(&target[..len - 4]);
-        println!("seq: {}, hash: {}", self.seq, hash);
-        target[(len - 4)..len].copy_from_slice(&hash.to_le_bytes());
+        self.file.write_all(&footer_buf)?;
 
-        Ok(len)
+        Ok(())
     }
 }
 
-static SEQ: AtomicU64 = AtomicU64::new(1);
-
-pub fn main() {
-    let mut file = [0u8; 65_536];
+pub fn main() -> io::Result<()> {
+    let mut cnt = 0;
+    let mut txn_buf = vec![0u8; 255 * 64 * 1024];
     let mut write_cursor: usize = 0;
-    let mut buf = [0u8; 4096];
+    let mut rec_buf = [0u8; 64 * 1024];
+    let mut file_list: Vec<DbFile> = Vec::with_capacity(10);
 
+    if file_list.is_empty() {
+        file_list.push(DbFile::new(
+            REC_SEQ.load(std::sync::atomic::Ordering::Relaxed),
+        )?);
+    }
+
+    let file = file_list.last_mut().unwrap();
+    let start = Instant::now();
     loop {
-        let seq = SEQ.load(std::sync::atomic::Ordering::Relaxed);
-        let rand = rand::random::<u8>() as usize;
-        rand::fill(&mut buf[..rand]);
+        let txn_seq = REC_SEQ.load(std::sync::atomic::Ordering::SeqCst);
+        let txn_cnt = rand::random_range(1..=20);
+        for _ in 0..txn_cnt {
+            let seq = REC_SEQ.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+            let rand = rand::random::<u16>() as usize;
+            rand::fill(&mut rec_buf[..rand]);
 
-        let rec = Record::new(seq, 1, 1, &buf[..rand]);
-        write_cursor += rec.write(&mut file[write_cursor..]).unwrap();
-        println!("cursor: {}", write_cursor);
+            let rec = Record::new(seq, txn_seq, txn_cnt, 1, &rec_buf[..rand]);
+            write_cursor += rec.write(&mut txn_buf[write_cursor..]).unwrap();
+        }
 
-        if seq >= 10 {
+        if !file.has_space(write_cursor) {
+            file.seal()?;
             break;
         }
-    }
-    println!("wrote {}bytes to file buf", write_cursor);
 
-    let mut read_cursor = 0_usize;
-    while read_cursor < write_cursor {
-        let rec = match Record::from_slice(&file[read_cursor..]) {
-            Ok(rec) => {
-                let computed_hash = crc32c(&file[read_cursor..(read_cursor + rec.len() - 4)]);
-                if computed_hash != rec.hash {
-                    eprintln!("hashes do not match");
-                }
-                rec
-            }
-            Err(e) => {
-                eprintln!("Failed getting record from slice: {e}");
-                break;
-            }
-        };
+        file.write(
+            &txn_buf[..write_cursor],
+            REC_SEQ.load(std::sync::atomic::Ordering::Relaxed) - 1,
+        )?;
+        cnt += 1;
+        println!("write cursor on: {}; txn_cnt at: {}", file.written, cnt);
 
-        println!("seq: {}, hash: {}", rec.seq, rec.hash);
-        read_cursor += rec.len();
+        write_cursor = 0;
     }
+
+    let elapsed = start.elapsed();
+
+    println!("run took {:.3?}", elapsed);
+    println!(
+        "{} records -> {:.0} records/s",
+        file.max_seq,
+        file.max_seq as f64 / elapsed.as_secs_f64()
+    );
+    println!(
+        "{} txn -> {:.0} txn/s",
+        cnt,
+        cnt as f64 / elapsed.as_secs_f64()
+    );
+    println!(
+        "write speed {:.2}mB/s",
+        (file.written as f64 / (1024.0 * 1024.0)) / elapsed.as_secs_f64()
+    );
+
+    Ok(())
 }
